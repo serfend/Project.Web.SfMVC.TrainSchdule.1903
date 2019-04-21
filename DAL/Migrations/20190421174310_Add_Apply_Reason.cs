f22b031ccba9b71c7caf02b2da14e7828d1c5686
汉广 <liuchangm7j@vip.qq.com>
2019-04-22 01:48:44 +0800

    new 同步Reason列

---
+++ DAL/Migrations/20190421174310_Add_Apply_Reason.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Add_Apply_Reason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reason",
+                table: "Applies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reason",
+                table: "Applies");
+        }
+    }
+}
