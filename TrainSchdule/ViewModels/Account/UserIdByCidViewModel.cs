513fa047f3af24eb2f7f14510559393acfe750c1
汉广 <liuchangm7j@vip.qq.com>
2019-05-10 10:52:37 +0800

    data

--- TrainSchdule/ViewModels/Account/UserIdByCidViewModel.cs
+++ TrainSchdule/ViewModels/Account/UserIdByCidViewModel.cs
@@ -6,13 +6,25 @@ using TrainSchdule.ViewModels.System;
 
 namespace TrainSchdule.ViewModels.Account
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	public class UserIdByCidViewModel:ApiDataModel
 	{
+		/// <summary>
+		/// 
+		/// </summary>
 		public UserIdByCidDataModel Data { get; set; }
 	}
 
+	/// <summary>
+	/// 
+	/// </summary>
 	public class UserIdByCidDataModel
 	{
+		/// <summary>
+		/// 
+		/// </summary>
 		public string Id { get; set; }
 	}
 }
