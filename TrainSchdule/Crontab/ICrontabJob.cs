18e764c7389455116aac44512751ace1ea70589c
汉广 <liuchangm7j@vip.qq.com>
2019-11-10 15:20:33 +0800

    Create ICrontabJob.cs

---
+++ TrainSchdule/Crontab/ICrontabJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainSchdule.Crontab
+{
+	/// <summary>
+	/// 定时工作任务
+	/// </summary>
+	public interface ICrontabJob
+	{
+		/// <summary>
+		/// 执行任务
+		/// </summary>
+		 void Run();
+	}
+}
