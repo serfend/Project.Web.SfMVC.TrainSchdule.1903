07d4fcfa9efa55ff13be366cc335507712024fb6
汉广 <liuchangm7j@vip.qq.com>
2019-11-17 08:54:56 +0800

    append phy

---
+++ DAL/Entities/ZX/Phy/Standard.cs
@@ -0,0 +1,109 @@
+﻿using DAL.Entities.UserInfo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Entities.ZX.Phy
+{
+	public class Standard : BaseEntity
+	{
+		[NotMapped]
+		[JsonIgnore]
+		public virtual Subject BelongTo { get; set; }
+		[NotMapped]
+		[JsonIgnore]
+		public SortedDictionary<int, int> GradePairsInner { get; set; }
+		private string GradePairsStr;
+		/// <summary>
+		/// 分数对     成绩:得分|成绩:得分|成绩:得分|...
+		/// </summary>
+		public string GradePairs
+		{
+			get => GradePairsStr;
+			set
+			{
+				GradePairsStr = value;
+				if (BelongTo == null) return;
+				GradePairsInner = new SortedDictionary<int, int>();
+				var list = value.Split('|');
+				foreach (var p in list)
+				{
+					var i = p.Split(':');
+					var key = this.ToValue(i[0]);
+					if (i.Length == 2) GradePairsInner[key] = Convert.ToInt32(i[1]);
+				}
+				if (BaseStandard == 0) BaseStandard = 60;
+			}
+		}
+		public string ExpressionWhenFullGrade { get; set; }
+		public int minAge { get; set; }
+		public int maxAge { get; set; }
+		public GenderEnum gender { get; set; }
+		/// <summary>
+		/// 达到及格所需成绩
+		/// </summary>
+		public int BaseStandard { get; set; }
+	}
+	public enum ValueFormat
+	{
+		Default,
+		TimeBase,
+		SecondBase
+	}
+	public static class StandardExtensions
+	{
+		public static int ToValue(this Standard standard, string raw)
+		{
+			if (standard == null) return 0;
+			switch (standard.BelongTo.ValueFormat)
+			{
+				case ValueFormat.Default: return Convert.ToInt32(raw);
+				case ValueFormat.SecondBase:
+				case ValueFormat.TimeBase:
+					{
+						var t = new TimeSpan();
+						var minute = raw.Split('\'');
+						if (minute.Length > 1)
+						{
+							t = t.Add(TimeSpan.FromMinutes(Convert.ToInt32(minute[0])));
+							raw = minute[1];
+						}
+						t = t.Add(TimeSpan.FromMilliseconds(Convert.ToInt32(Convert.ToDecimal(raw) * 1000)));
+						return (int)(t.TotalMilliseconds * (standard.BelongTo.CountDown ? -1 : 1));
+					}
+			}
+			return 0;
+		}
+		public static string ToRawValue(this Standard standard,int expectGrade=0)
+		{
+			if (standard == null) return "无此科目";
+			int value = 0;
+			if (expectGrade == 0) expectGrade = standard.BaseStandard;
+			bool anyMatch = false;
+			foreach (var v in standard.GradePairsInner) if (v.Value> expectGrade){ value = v.Key; anyMatch = true; break; }
+			if(!anyMatch)value=standard.GradePairsInner.Keys.Max();
+			value = Math.Abs(value);
+			switch (standard.BelongTo.ValueFormat)
+			{
+				case ValueFormat.Default: return Convert.ToString(value);
+				case ValueFormat.SecondBase:
+				case ValueFormat.TimeBase:
+					{
+						var t = TimeSpan.FromMilliseconds(value);
+						return $"{Math.Floor(t.TotalMinutes)}'{t.Seconds}";
+					}
+			}
+			return "未设计分法";
+		}
+		public static int Age(this DateTime birthDay)
+		{
+			DateTime now = DateTime.Now;
+			int age = now.Year - birthDay.Year;
+			if (birthDay > now.AddYears(-age)) age--;
+			return age;
+		}
+	}
+}
---
+++ DAL/Entities/ZX/Phy/Subject.cs
@@ -0,0 +1,31 @@
+﻿using DAL.Entities.UserInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Entities.ZX.Phy
+{
+	public class Subject : BaseEntity
+	{
+		/// <summary>
+		/// 科目名称
+		/// </summary>
+		public string Name { get; set; }
+		public ValueFormat ValueFormat { get; set; }
+		/// <summary>
+		/// 是否为值越小成绩越好
+		/// </summary>
+		public bool CountDown { get; set; }
+		private IEnumerable<Standard> standards;
+		public virtual IEnumerable<Standard> Standards
+		{
+			get => standards; set
+			{
+				foreach (var i in value) { i.BelongTo = this; i.GradePairs = i.GradePairs; }
+				standards = value;
+			}
+
+		}
+	}
+}
