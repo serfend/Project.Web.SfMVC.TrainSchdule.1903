175d176ae17e7021f1ee83752cd31310476a49b3
汉广 <liuchangm7j@vip.qq.com>
2019-11-17 08:55:51 +0800

    modefy hangfire apply root

--- TrainSchdule/System/HangfireAuthorizeFilter.cs
+++ TrainSchdule/System/HangfireAuthorizeFilter.cs
@@ -1,18 +1,34 @@
-﻿using Hangfire.Annotations;
+﻿using BLL.Interfaces;
+using BLL.Services;
+using Hangfire.Annotations;
 using Hangfire.Dashboard;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TrainSchdule.ViewModels.Verify;
 
 namespace TrainSchdule.System
 {
 	public class HangfireAuthorizeFilter : IDashboardAuthorizationFilter
 	{
+		private IGoogleAuthService googleAuthService;
+
+		public HangfireAuthorizeFilter()
+		{
+			this.googleAuthService = new GoogleAuthService();
+		}
+
 		public bool Authorize([NotNull] DashboardContext context)
 		{
 			var httpcontext = context.GetHttpContext();
-			return httpcontext.User.Identity.IsAuthenticated&&httpcontext.User.Identity.Name=="1000000";
+			var auth = httpcontext.Request.Cookies?["Auth"];
+			if (auth != null) return new GoogleAuthViewModel()
+			{
+				AuthByUserID="root",
+				Code=Convert.ToInt32(auth)
+			}.Verify(googleAuthService);
+			return (httpcontext.User.Identity.IsAuthenticated && httpcontext.User.Identity.Name == "1000000");
 		}
 	}
 }
