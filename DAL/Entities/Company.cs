b6bc3d719d5b8a3640f55d3d997f37daa6e8d2b0
汉广 <liuchangm7j@vip.qq.com>
2019-11-24 00:36:17 +0800

    new CompanyLevel

--- DAL/Entities/Company.cs
+++ DAL/Entities/Company.cs
@@ -6,15 +6,34 @@ namespace DAL.Entities
 
 	public class Company
 	{
-		public string Name { get; set; }
-		public bool IsPrivate { get; set; }
 		/// <summary>
 		/// 单位代码
 		/// </summary>
 		[DatabaseGenerated(DatabaseGeneratedOption.None)]
 		[Key]
-		public string Code{get; set;}
+		public string Code { get; set; }
+		public string Name { get; set; }
+
+		public CompanyLevel Level { get; set; }
+		public bool IsPrivate { get; set; }
 		public string Description { get; set; }
 	}
-		
+	public enum CompanyLevel
+	{
+		JW,
+		DJQ,
+		FJQ,
+		ZJ,
+		FJ,
+		ZS,
+		FS,
+		ZT,
+		FT,
+		ZY,
+		FY,
+		ZL,
+		FL,
+		ZP,
+		None
+	}
 }
