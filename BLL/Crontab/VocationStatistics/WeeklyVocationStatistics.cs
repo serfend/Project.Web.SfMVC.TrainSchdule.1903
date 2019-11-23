cfc967001fcf8cd82ca41d8985011ce46af96e5e
汉广 <liuchangm7j@vip.qq.com>
2019-11-24 00:37:01 +0800

    modefu crontab statistics

--- BLL/Crontab/VocationStatistics/WeeklyVocationStatistics.cs
+++ BLL/Crontab/VocationStatistics/WeeklyVocationStatistics.cs
@@ -10,13 +10,13 @@ namespace TrainSchdule.Crontab
 	/// <summary>
 	/// 每周休假情况统计
 	/// </summary>
-	public class WeeklyVocationStatstics:BaseOnTimeVocationStatistics
+	public class WeeklyVocationStatistics:BaseOnTimeVocationStatistics
 	{
 		private readonly ApplicationDbContext _context;
 
-		public static DateTime Start = DateTime.Today.AddMonths(-1);
+		public static DateTime Start = DateTime.Today.AddDays(-1);
 		public static DateTime End = DateTime.Today;
-		public WeeklyVocationStatstics(ApplicationDbContext context):base(context,Start,End,$"{Start.Year}_{new GregorianCalendar().GetWeekOfYear(Start,CalendarWeekRule.FirstFullWeek,DayOfWeek.Monday) + 1}")
+		public WeeklyVocationStatistics(ApplicationDbContext context):base(context,Start,End,$"{Start.Year}_{new GregorianCalendar().GetWeekOfYear(Start,CalendarWeekRule.FirstFullWeek,DayOfWeek.Monday) + 1}")
 		{
 			_context = context;
 		}
---
+++ BLL/Crontab/VocationStatistics/YearlyVocationStatistics.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainSchdule.Crontab
+{
+	/// <summary>
+	/// 每年休假情况统计
+	/// </summary>
+	public class YearlyVocationStatistics : BaseOnTimeVocationStatistics
+	{
+		private readonly ApplicationDbContext _context;
+
+		public static DateTime Start = DateTime.Today.AddYears(-1);
+		public static DateTime End = DateTime.Today;
+		public YearlyVocationStatistics(ApplicationDbContext context) : base(context, Start, End, $"{Start.Year}_{new GregorianCalendar().GetWeekOfYear(Start, CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday) + 1}")
+		{
+			_context = context;
+		}
+
+	}
+}
