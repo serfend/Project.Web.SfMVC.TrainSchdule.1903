5b43a2165febec14b89c4c63a3279e9126f8dcc1
汉广 <liuchangm7j@vip.qq.com>
2019-11-24 13:28:27 +0800

    new 统计算法

---
+++ BLL/Extensions/VocationStatisticsExtensions.cs
@@ -0,0 +1,138 @@
+﻿using BLL.Helpers;
+using DAL.Data;
+using DAL.Entities;
+using DAL.Entities.ApplyInfo;
+using DAL.Entities.UserInfo;
+using DAL.Entities.Vocations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Extensions
+{
+	public static class VocationStatisticsExtensions
+	{
+		public static void StatisticsInit(ref VocationStatisticsDescription model, ApplicationDbContext context, int currentYear,string statisticsId)
+		{
+			if (context == null) return;
+			if (model == null) return;
+			model.StatisticsId = statisticsId;
+			var tmpList = new List<VocationStatisticsDescription>();
+			foreach (var m in model.Childs)
+			{
+				VocationStatisticsDescription tmp=m;
+				StatisticsInit(ref tmp, context, currentYear, statisticsId);
+				tmpList.Add(tmp);
+			}
+			model.Childs = tmpList;
+			CaculateCurrentLevel(ref model, context, currentYear);
+			CaculateChildLevel(ref model, context, currentYear);
+
+		}
+		private static void CaculateCurrentLevel(ref VocationStatisticsDescription model, ApplicationDbContext context,int currentYear)
+		{
+			int ApplyCountAccess=0, ApplyCountAuditing=0, ApplyCountDeny=0, ApplyMembersCountAccess=0, ApplyMembersCountAuditing=0, ApplyMembersCountDeny=0, ApplySumDayCountAccess=0, ApplySumDayCountAuditing=0, ApplySumDayCountDeny=0, MembersCount=0, CompleteYearlyVocationCount=0, MembersVocationDayLessThanP60=0, CompleteVocationExpectDayCount=0, CompleteVocationRealDayCount=0;
+			foreach (var a in model.Applies)
+			{
+				if (a.Status == AuditStatus.NotPublish || a.Status == AuditStatus.NotSave || a.Status == AuditStatus.Withdrew) continue;
+				else
+				{
+					if (a.Status == AuditStatus.Denied)
+					{
+						ApplyCountDeny++;//时间段内未通过审批的申请数量
+					}
+					else if (a.Status == AuditStatus.Accept)
+					{
+						ApplyCountAccess++;
+						var stampReturn = a.RequestInfo.StampLeave.Value.AddDays(a.RequestInfo.VocationLength) > DateTime.Now ? DateTime.Now : a.RequestInfo.StampReturn.Value;//截止今日
+						CompleteVocationRealDayCount += stampReturn.Subtract(a.RequestInfo.StampLeave.Value).Days;//时间段内休假天数
+					}
+					else
+					{
+						ApplyCountAuditing++;//时间段内审批中的申请
+					}
+				}
+			}
+			var companyCode = model.Company.Code;
+			foreach (var member in context.AppUsers.Where<User>(u => u.CompanyInfo.Company.Code == companyCode))
+			{
+				MembersCount++;
+				CompleteVocationExpectDayCount += member.SocialInfo.Settle.PrevYearlyLength;//单位全年应休假天数
+				var membersApplies = context.Applies.Where<Apply>(a => a.BaseInfo.From.Id == member.Id&&a.Create.Value.Year== currentYear);
+				//全年休假天数
+				var memberCompleteVocation = membersApplies.Sum<Apply>(a => a.Status == AuditStatus.Accept ? a.RequestInfo.VocationLength : 0);
+				if (memberCompleteVocation >= member.SocialInfo.Settle.PrevYearlyLength) CompleteYearlyVocationCount++;
+				else if (memberCompleteVocation < 0.6f * member.SocialInfo.Settle.PrevYearlyLength) MembersVocationDayLessThanP60++;
+
+				  ApplySumDayCountAccess += memberCompleteVocation;
+				ApplySumDayCountDeny = membersApplies.Sum<Apply>(a => a.Status == AuditStatus.Denied ? a.RequestInfo.VocationLength : 0);
+				ApplySumDayCountAuditing = membersApplies.Sum<Apply>(a => a.Status == AuditStatus.Auditing ? a.RequestInfo.VocationLength : 0);
+
+				membersApplies = model.Applies.Where<Apply>(a => a.BaseInfo.From
+				  .Id == member.Id);
+				if (membersApplies.Any<Apply>(a=>a.Status==AuditStatus.Accept)) ApplyMembersCountAccess++;
+				if (membersApplies.Any<Apply>(a=>a.Status==AuditStatus.Denied)) ApplyMembersCountDeny++;
+				if (membersApplies.Any<Apply>(a=>a.Status==AuditStatus.Auditing)) ApplyMembersCountAuditing++;
+				
+			}
+			VocationStatisticsData tmp=null;
+			InputStatisticsData(ref tmp,ApplyCountAccess, ApplyCountAuditing, ApplyCountDeny, ApplyMembersCountAccess, ApplyMembersCountAuditing, ApplyMembersCountDeny, ApplySumDayCountAccess, ApplySumDayCountAuditing, ApplySumDayCountDeny, MembersCount, CompleteYearlyVocationCount, MembersVocationDayLessThanP60, CompleteVocationExpectDayCount, CompleteVocationRealDayCount);
+			model.CurrentLevelStatistics = tmp;
+		}
+		private static void CaculateChildLevel(ref VocationStatisticsDescription model, ApplicationDbContext context, int currentYear)
+		{
+			int ApplyCountAccess = 0, ApplyCountAuditing = 0, ApplyCountDeny = 0, ApplyMembersCountAccess = 0, ApplyMembersCountAuditing = 0, ApplyMembersCountDeny = 0, ApplySumDayCountAccess = 0, ApplySumDayCountAuditing = 0, ApplySumDayCountDeny = 0, MembersCount = 0, CompleteYearlyVocationCount = 0, MembersVocationDayLessThanP60 = 0, CompleteVocationExpectDayCount = 0, CompleteVocationRealDayCount = 0;
+			ApplyCountAccess = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplyCount.Access);
+			ApplyCountAuditing = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplyCount.Auditing);
+			ApplyCountDeny = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplyCount.Deny);
+			ApplyMembersCountAccess = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplyMembersCount.Access);
+			ApplyMembersCountAuditing = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplyMembersCount.Auditing);
+			ApplyMembersCountDeny = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplyMembersCount.Deny);
+			ApplySumDayCountAccess = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplySumDayCount.Access);
+			ApplySumDayCountAuditing = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplySumDayCount.Auditing);
+			ApplySumDayCountDeny = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.ApplySumDayCount.Deny);
+			MembersCount = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.MembersCount);
+			CompleteYearlyVocationCount = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.CompleteYearlyVocationCount);
+			MembersVocationDayLessThanP60 = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.MembersVocationDayLessThanP60);
+			CompleteVocationExpectDayCount = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.CompleteVocationExpectDayCount);
+			CompleteVocationRealDayCount = model.CurrentLevelStatistics.MembersCount + model.Childs.Sum<VocationStatisticsDescription>(v => v.IncludeChildLevelStatistics.CompleteVocationRealDayCount);
+
+			VocationStatisticsData tmp = null;
+			InputStatisticsData(ref tmp, ApplyCountAccess, ApplyCountAuditing, ApplyCountDeny, ApplyMembersCountAccess, ApplyMembersCountAuditing, ApplyMembersCountDeny, ApplySumDayCountAccess, ApplySumDayCountAuditing, ApplySumDayCountDeny, MembersCount, CompleteYearlyVocationCount, MembersVocationDayLessThanP60, CompleteVocationExpectDayCount, CompleteVocationRealDayCount);
+			model.IncludeChildLevelStatistics = tmp;
+			}
+  private static void InputStatisticsData(ref VocationStatisticsData model,int ApplyCountAccess,int ApplyCountAuditing,int ApplyCountDeny,int ApplyMembersCountAccess,int ApplyMembersCountAuditing,int ApplyMembersCountDeny,int ApplySumDayCountAccess,int ApplySumDayCountAuditing,int ApplySumDayCountDeny,int MembersCount,int CompleteYearlyVocationCount,int MembersVocationDayLessThanP60,int CompleteVocationExpectDayCount,int CompleteVocationRealDayCount)
+		{
+
+
+			model = new VocationStatisticsData()
+			{
+				ApplyCount = new VocationStatisticsDescriptionDataStatusCount()
+				{
+					Access =ApplyCountAccess,
+					Auditing =ApplyCountAuditing,
+					Deny =ApplyCountDeny
+				},
+				ApplyMembersCount = new VocationStatisticsDescriptionDataStatusCount()
+				{
+					Access =ApplyMembersCountAccess,
+					Auditing =ApplyMembersCountAuditing,
+					Deny =ApplyMembersCountDeny
+				},
+				ApplySumDayCount = new VocationStatisticsDescriptionDataStatusCount()
+				{
+					Access =ApplySumDayCountAccess,
+					Auditing =ApplySumDayCountAuditing,
+					Deny =ApplySumDayCountDeny
+				},
+				MembersCount =MembersCount,
+				CompleteYearlyVocationCount =CompleteYearlyVocationCount,
+				MembersVocationDayLessThanP60 =MembersVocationDayLessThanP60,
+				CompleteVocationExpectDayCount =CompleteVocationExpectDayCount,
+				CompleteVocationRealDayCount =CompleteVocationRealDayCount
+			};
+		}
+
+	}
+}
--- DAL/Entities/Vocations/VocationStatistics.cs
+++ DAL/Entities/Vocations/VocationStatistics.cs
@@ -2,37 +2,96 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text;
 
 namespace DAL.Entities.Vocations
 {
-	public class VocationStatistics:BaseEntity
+	public class VocationStatistics
 	{
-		public string Code { get; set; }
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string Id { get; set; }
 		public DateTime Start { get; set; }
 		public DateTime End { get; set; }
-		public virtual IEnumerable<VocationStatisticsDescription> vocationStatisticsDescription { get; set; }
+		/// <summary>
+		/// 统计期间使用的年度
+		/// </summary>
+		public int CurrentYear { get; set; }
+		/// <summary>
+		/// 通常自动生成，也可手动修改
+		/// </summary>
+		public string Description { get; set; }
+		public virtual VocationStatisticsDescription RootCompanyStatistics { get; set; }
 	}
-	public class VocationStatisticsDescription
+	public class VocationStatisticsDescription:BaseEntity
 	{
+		public string StatisticsId { get; set; }
 		/// <summary>
 		/// 单位下属单位休假情况
 		/// </summary>
-		public virtual IEnumerable<VocationDescription> Childs { get; set; }
+		public virtual IEnumerable<VocationStatisticsDescription> Childs { get; set; }
 		/// <summary>
-		/// 单位名称
+		/// 单位
 		/// </summary>
-		public string CompanyFullName { get; set; }
+		public virtual Company Company { get; set; }
+
 		/// <summary>
-		/// 单位本级人员数量
+		/// 单位本级的休假记录
+		/// </summary>
+		[NotMapped]
+		public virtual IQueryable<ApplyInfo.Apply> Applies { get; set; }
+		/// <summary>
+		/// 单位本级数据
+		/// </summary>
+		public virtual VocationStatisticsData CurrentLevelStatistics { get; set; }
+
+		/// <summary>
+		/// 包含子单位数据
+		/// </summary>
+		public virtual VocationStatisticsData IncludeChildLevelStatistics { get; set; }
+	}
+	public class VocationStatisticsData:BaseEntity
+	{
+
+		/// <summary>
+		/// 单位本级人员数量<see cref="单位人员总数"/>
 		/// </summary>
 		public int MembersCount { get; set; }
-		public virtual IEnumerable<ApplyInfo.Apply> Applies { get; set; }
-		//单位人员总数
-		//休满假数（率）
-		//应（实）休总天数
-		//未休满假数（率）
-		//休假率低于60%数
+		/// <summary>
+		/// 已完成全年休假指标的人数<see cref="休满假数（率）"/>
+		/// </summary>
+		public int CompleteYearlyVocationCount { get; set; }
+		/// <summary>
+		/// 应当休假总天数<see cref="应（实）休总天数"/>
+		/// </summary>
+		public int CompleteVocationExpectDayCount { get; set; }
+		/// <summary>
+		/// 实际休假总天数<see cref="应（实）休总天数"/>
+		/// </summary>
+		public int CompleteVocationRealDayCount { get; set; }
+		/// <summary>
+		/// 休假率低于60%人员数<see cref="休假率低于60%数"/>
+		/// </summary>
+		public int MembersVocationDayLessThanP60 { get; set; }
+		/// <summary>
+		/// 新增申请的数量
+		/// </summary>
+		public virtual VocationStatisticsDescriptionDataStatusCount ApplyCount { get; set; }
+		/// <summary>
+		/// 新增申请的人数
+		/// </summary>
+		public virtual VocationStatisticsDescriptionDataStatusCount ApplyMembersCount { get; set; }
+		/// <summary>
+		/// 申请天数
+		/// </summary>
+		public virtual VocationStatisticsDescriptionDataStatusCount ApplySumDayCount { get; set; }
 	}
-
+	public class VocationStatisticsDescriptionDataStatusCount : BaseEntity
+	{
+		public int Access { get; set; }
+		public int Deny { get; set; }
+		public int Auditing { get; set; }
+		public int Sum { get => Access + Deny + Auditing;  }
+	}
 }
