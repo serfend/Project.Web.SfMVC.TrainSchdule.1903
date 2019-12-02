f957a5ccaea7c072c881d07d80bf01a18573c778
汉广 <liuchangm7j@vip.qq.com>
2019-12-02 18:28:24 +0800

    tmp weeklygrade

---
+++ DAL/Entities/UserInfo/UserGrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities.UserInfo
+{
+	public class UserGrade:BaseEntity
+	{
+		/// <summary>
+		/// 成绩创建时间
+		/// </summary>
+		public DateTime Create { get; set; }
+
+	}
+
+}
