80b101cd4f4f9133c49f27e49d0277cb39181720
汉广 <liuchangm7j@vip.qq.com>
2020-01-21 07:47:21 +0800

    new 支持自定义状态

--- BLL/Helpers/ActionStatusMessage.cs
+++ BLL/Helpers/ActionStatusMessage.cs
@@ -18,6 +18,18 @@
 			this.Status = status;
 			this.Message = message;
 		}
+		/// <summary>
+		/// 在原报错信息基础上附加信息
+		/// </summary>
+		/// <param name="status"></param>
+		/// <param name="message"></param>
+		/// <param name="AppendToRawMessage"></param>
+		public ApiResult(ApiResult status, string message, bool AppendToRawMessage)
+		{
+			this.status = status?.status??-25844;
+			this.message = status?.message??"未定义类型";
+			if (AppendToRawMessage) this.message += $" {AppendToRawMessage}";
+		}
 		public ApiResult() { }
 	}
 }
