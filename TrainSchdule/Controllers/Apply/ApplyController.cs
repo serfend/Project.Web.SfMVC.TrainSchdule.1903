bcc08e436e90702a301bc3323d2654d060b3181d
汉广 <liuchangm7j@vip.qq.com>
2020-04-10 14:57:39 +0800

    modefy 仅有权限人可删除申请

--- TrainSchdule/Controllers/Apply/ApplyController.cs
+++ TrainSchdule/Controllers/Apply/ApplyController.cs
@@ -215,7 +215,7 @@ namespace TrainSchdule.Controllers.Apply
 			Guid.TryParse(model.Id, out var id);
 			var apply = _applyService.GetById(id);
 			if (apply == null) return new JsonResult(ActionStatusMessage.Apply.NotExist);
-			if (authByUser.Id != apply.BaseInfo.From.Id && !_userActionServices.Permission(authByUser.Application.Permission, DictionaryAllPermission.Apply.Default, Operation.Update, authByUser.Id, apply.BaseInfo.From.CompanyInfo.Company.Code)) return new JsonResult(ActionStatusMessage.Account.Auth.Invalid.Default);
+			if (!_userActionServices.Permission(authByUser.Application.Permission, DictionaryAllPermission.Apply.Default, Operation.Update, authByUser.Id, apply.BaseInfo.From.CompanyInfo.Company.Code)) return new JsonResult(ActionStatusMessage.Account.Auth.Invalid.Default);
 			var ua = _userActionServices.Log(UserOperation.RemoveApply, apply.BaseInfo.From.Id, $"通过{authByUser.Id}移除{apply.Create}创建的{apply.RequestInfo.VocationLength}天休假申请");
 			//if (!(apply.Status == AuditStatus.NotPublish || apply.Status == AuditStatus.NotSave)) return new JsonResult(ActionStatusMessage.Apply.Operation.StatusInvalid.CanNotDelete);
 			await _applyService.Delete(apply);
