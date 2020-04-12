d01db70db633879b16cf182919bb61e9019ae143
serfend <serfend@foxmail.com>
2020-04-13 01:25:06 +0800

    remvoe exist filed

---
+++ DAL/Migrations/20200412172337_n413.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class n413 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Exist",
+                table: "UserFileInfos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Exist",
+                table: "UserFileInfos",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
