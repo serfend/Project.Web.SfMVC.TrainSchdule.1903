fefaae2ed98051286b37e852e227a1324b93f7ee
serfend <serfend@foxmail.com>
2020-04-20 09:23:35 +0800

    Update ICompanyManagerServices.cs

--- BLL/Interfaces/ICompanyManagerServices.cs
+++ BLL/Interfaces/ICompanyManagerServices.cs
@@ -10,19 +10,24 @@ namespace BLL.Interfaces
 	public interface ICompanyManagerServices
 	{
 		CompanyManagers GetById(Guid id);
+
 		IEnumerable<CompanyManagers> GetAll(Expression<Func<CompanyManagers, bool>> predicate, int page, int pageSize);
+
 		int Edit(CompanyManagers model);
+
 		CompanyManagers CreateManagers(CompanyManagerVdto model);
+
 		CompanyManagers Create(CompanyManagers model);
+
 		int Delete(CompanyManagers model);
+
 		/// <summary>
-		/// 通过用户名获取管理员
+		/// 获取单位所有的管理员
 		/// </summary>
-		/// <param name="userId"></param>
 		/// <param name="companyCode"></param>
 		/// <returns></returns>
-		CompanyManagers GetManagerByUC(string userId, string companyCode);
+		IEnumerable<CompanyManagers> GetManagers(string companyCode);
 
-		IEnumerable<User> GetMembers(string code, int page, int pageSize,out int totalCount);
+		IEnumerable<User> GetMembers(string code, int page, int pageSize, out int totalCount);
 	}
 }
