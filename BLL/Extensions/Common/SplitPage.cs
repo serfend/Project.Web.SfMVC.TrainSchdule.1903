3da7a673eb405d17ebc7f2bdb22e187a88595dab
serfend <serfend@foxmail.com>
2020-05-03 21:44:44 +0800

    new pageHandler

---
+++ BLL/Extensions/Common/SplitPage.cs
@@ -0,0 +1,30 @@
+﻿using DAL.QueryModel;
+using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Extensions.Common
+{
+	public static class SplitPageExtensions
+	{
+		public static IQueryable<TSource> SplitPage<TSource>(this IQueryable<TSource> model, QueryByPage pages, out int totalCount)
+		{
+			if (model == null)
+			{
+				totalCount = 0;
+				return null;
+			}
+			if (pages == null) pages = new QueryByPage()
+			{
+				PageIndex = 0,
+				PageSize = 20
+			};
+			totalCount = model.Count();
+			if (pages.PageSize <= 0 || pages.PageIndex < 0) return model.Where(f => false);
+			var res = model.Skip(pages.PageIndex * pages.PageSize).Take(pages.PageSize);
+			return res;
+		}
+	}
+}
