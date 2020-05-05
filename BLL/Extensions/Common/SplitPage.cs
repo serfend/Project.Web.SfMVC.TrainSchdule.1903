3cdf996f23fa0020d6fc16debd924389bb903f06
serfend <serfend@foxmail.com>
2020-05-05 10:27:39 +0800

    modefy split page use tuple async

--- BLL/Extensions/Common/SplitPage.cs
+++ BLL/Extensions/Common/SplitPage.cs
@@ -4,27 +4,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BLL.Extensions.Common
 {
 	public static class SplitPageExtensions
 	{
-		public static IQueryable<TSource> SplitPage<TSource>(this IQueryable<TSource> model, QueryByPage pages, out int totalCount)
+		public static async Task<Tuple<IQueryable<TSource>, int>> SplitPage<TSource>(this IQueryable<TSource> model, QueryByPage pages)
 		{
 			if (model == null)
-			{
-				totalCount = 0;
 				return null;
-			}
 			if (pages == null) pages = new QueryByPage()
 			{
 				PageIndex = 0,
 				PageSize = 20
 			};
-			totalCount = model.Count();
-			if (pages.PageSize <= 0 || pages.PageIndex < 0) return model.Where(f => false);
+			var totalCount = await Task.Run<int>(() =>
+			{
+				return model.Count();
+			}).ConfigureAwait(true);
+			if (pages.PageSize <= 0 || pages.PageIndex < 0) return new Tuple<IQueryable<TSource>, int>(model.Where(f => false), 0);
 			var res = model.Skip(pages.PageIndex * pages.PageSize).Take(pages.PageSize);
-			return res;
+			return new Tuple<IQueryable<TSource>, int>(res, totalCount);
 		}
 	}
 }
