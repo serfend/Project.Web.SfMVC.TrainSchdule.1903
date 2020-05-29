9647c3c61ccf1730988abc4be59bd76405386940
serfend <serfend@foxmail.com>
2020-05-29 11:34:25 +0800

    refactor[settle]

---
+++ DAL/Migrations/20200529032813_SocialModefyRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class SocialModefyRecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRemoved",
+                table: "AppUsersSettleModefyRecord",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IsRemovedDate",
+                table: "AppUsersSettleModefyRecord",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRemoved",
+                table: "AppUsersSettleModefyRecord");
+
+            migrationBuilder.DropColumn(
+                name: "IsRemovedDate",
+                table: "AppUsersSettleModefyRecord");
+        }
+    }
+}
---
+++ TrainSchdule/Extensions/Common/EntityExtensions.cs
@@ -0,0 +1,30 @@
+﻿using BLL.Helpers;
+using DAL.Entities;
+using DAL.Entities.UserInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainSchdule.Extensions.Common
+{
+	/// <summary>
+	///
+	/// </summary>
+	public static class EntityExtensions
+	{
+		/// <summary>
+		/// 资源不存在
+		/// </summary>
+		/// <param name="model"></param>
+		/// <returns></returns>
+		public static ApiResult NotExist(this BaseEntity model) => ActionStatusMessage.Static.ResourceNotExist;
+
+		/// <summary>
+		/// 用户不存在
+		/// </summary>
+		/// <param name="model"></param>
+		/// <returns></returns>
+		public static ApiResult NotExist(this User model) => ActionStatusMessage.UserMessage.NotExist;
+	}
+}
