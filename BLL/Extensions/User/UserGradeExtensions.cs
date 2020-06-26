c46bb849f2b5cc00858a6af99f9e62414acdbbaf
serfend <serfend@foxmail.com>
2020-06-27 07:24:19 +0800

    feat[grade]filter by age&gender

---
+++ BLL/Extensions/User/UserGradeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities.UserInfo;
+using DAL.Entities.ZX.Phy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Extensions
+{
+	public static class UserGradeExtensions
+	{
+		public static IQueryable<GradePhySubject> GetSubjectsByUser(this IQueryable<GradePhySubject> list, UserBaseInfo userBase)
+		{
+			if (userBase == null) return null;
+			var userAge = userBase.Time_BirthDay.Age();
+			return list.Where(s => s.Standards.Any(d => d.minAge <= userAge && d.maxAge >= userAge && d.gender == userBase.Gender));
+		}
+	}
+}
