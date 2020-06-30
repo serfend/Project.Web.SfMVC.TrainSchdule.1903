9f4573d5b0ef0daaaf9c5c6086e06f5fdfbbf3bf
serfend <serfend@foxmail.com>
2020-06-30 14:53:26 +0800

    feat[common]init data dictionary

---
+++ DAL/Entities/Common/DataDictionary/CommonDataDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities.Common
+{
+	public class CommonDataDictionary : BaseEntityInt
+	{
+		/// <summary>
+		/// 字典组
+		/// </summary>
+		public string Group { get; set; }
+	}
+}
---
+++ DAL/Entities/Common/DataDictionary/CommonDataGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities.Common.DataDictionary
+{
+	public class CommonDataGroup : BaseEntityInt
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime Create { get; set; }
+	}
+}
