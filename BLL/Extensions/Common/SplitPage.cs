96d82b456ca910000beff426045cf15af39bb2ca
serfend <serfend@foxmail.com>
2020-08-17 15:42:17 +0800

    feat[common]split-page support as ienumer

--- BLL/Extensions/Common/SplitPage.cs
+++ BLL/Extensions/Common/SplitPage.cs
@@ -12,6 +12,16 @@ namespace BLL.Extensions.Common
 	{
 		public static Tuple<IQueryable<TSource>, int> SplitPage<TSource>(this IQueryable<TSource> model, int pageIndex = 0, int pageSize = 20) => model.SplitPage(new QueryByPage() { PageIndex = pageIndex, PageSize = pageSize });
 
+		public static Tuple<IEnumerable<TSource>, int> SplitPage<TSource>(this IEnumerable<TSource> model, QueryByPage pages)
+		{
+			if (model == null)
+				return null;
+			pages = pages.ValidSplitPage();
+			var totalCount = model.Count();
+			var res = pages.PageSize < 0 || pages.PageIndex < 0 ? model : model.Skip(pages.PageIndex * pages.PageSize).Take(pages.PageSize);
+			return new Tuple<IEnumerable<TSource>, int>(res, totalCount);
+		}
+
 		public static Tuple<IQueryable<TSource>, int> SplitPage<TSource>(this IQueryable<TSource> model, QueryByPage pages)
 		{
 			if (model == null)
