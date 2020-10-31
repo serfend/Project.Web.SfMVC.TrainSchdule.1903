a706ee117d947b3adf2b573ecf6e3dd3240c4f7f
serfend <serfend@foxmail.com>
2020-10-31 10:00:28 +0800

    refactor[user]typo

--- DAL/Entities/UserInfo/UserBase/UserBaseInfo.cs
+++ DAL/Entities/UserInfo/UserBase/UserBaseInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Entities.UserInfo
 {
@@ -45,6 +46,7 @@ namespace DAL.Entities.UserInfo
 		/// <summary>
 		/// 是否修改过密码
 		/// </summary>
+		[Column("PasswodModify")]
 		public bool PasswordModify { get; set; }
 	}
 
---
+++ DAL/Migrations/20201031015812_Company.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Company : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordModefy",
+                table: "AppUserBaseInfos",
+                newName: "PasswodModify");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswodModify",
+                table: "AppUserBaseInfos",
+                newName: "PasswordModefy");
+        }
+    }
+}
