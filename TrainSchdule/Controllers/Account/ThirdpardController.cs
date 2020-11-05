b0ac7a2a7aef99b0198581b91ce10674dab74025
serfend <serfend@foxmail.com>
2020-11-05 22:32:43 +0800

    feat[account]thirdpard account

---
+++ TrainSchdule/Controllers/Account/ThirdpardController.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainSchdule.Controllers.Account
+{
+	/// <summary>
+	///
+	/// </summary>
+	public partial class AccountController
+	{
+		/// <summary>
+		/// 修改第三方账号请求
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public IActionResult ThirdpardRequire()
+		{
+			return new JsonResult(ActionStatusMessage.Success);
+		}
+
+		/// <summary>
+		/// 确认修改第三方账号
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public IActionResult ThirdpardConfirm()
+		{
+			return new JsonResult(ActionStatusMessage.Success);
+		}
+	}
+}
---
+++ TrainSchdule/ViewModels/Account/ThirdpardVerifyDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainSchdule.ViewModels.Account
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ThirdpardVerifyDataModel
+	{
+	}
+}
