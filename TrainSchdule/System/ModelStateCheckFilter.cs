3dc5a55e1eac83e9b2b95ce202a01f118de782c0
汉广 <serfend@foxmail.com>
2021-03-29 18:55:50 +0800

    feat[model-check]log info if model-state error

--- TrainSchdule/System/ModelStateCheckFilter.cs
+++ TrainSchdule/System/ModelStateCheckFilter.cs
@@ -1,10 +1,12 @@
 ﻿using BLL.Helpers;
+using BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using TrainSchdule.ViewModels;
 
@@ -15,6 +17,15 @@ namespace TrainSchdule.System
 	/// </summary>
 	public class ModelStateCheckFilter : ActionFilterAttribute
 	{
+        private readonly IUserActionServices userActionServices;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ModelStateCheckFilter(IUserActionServices userActionServices)
+        {
+            this.userActionServices = userActionServices;
+        }
 		/// <summary>
 		///
 		/// </summary>
@@ -24,6 +35,9 @@ namespace TrainSchdule.System
 			if (!context.ModelState.IsValid)
 			{
 				var model = context.ModelState.ToModel();
+				var modelExcept = string.Join(";", model.Data.List.ToList().Select(o=>$"{o.Key}:{o.Message}"));
+				var route = context.HttpContext.Request.Path.Value;
+				userActionServices.Log(DAL.Entities.UserInfo.UserOperation.InvalidModel, context.HttpContext.User?.Identity?.Name, $"{route},{modelExcept}");
 				context.Result = new JsonResult(model);
 			}
 		}
