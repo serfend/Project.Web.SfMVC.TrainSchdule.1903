b5106e73f098fd6d29a0888700567dc34a2fc17f
汉广 <serfend@foxmail.com>
2021-05-19 19:11:11 +0800

    fix[apply-new]time interval show wrong

--- BLL/Services/ApplyServices/ApplyService.cs
+++ BLL/Services/ApplyServices/ApplyService.cs
@@ -54,7 +54,7 @@ namespace BLL.Services.ApplyServices
 			{
 				var time = appSetting.LastSubmitApplyTime ?? DateTime.MinValue;
 				// 若1分钟内连续提交两次，则下次提交限定到10分钟后
-				if (time > DateTime.Now.AddMinutes(10)) throw new ActionStatusMessageException(new ApiResult(ActionStatusMessage.ApplyMessage.Operation.Submit.Crash,$"请{time.Subtract(DateTime.Now.AddMinutes(10)).Seconds}秒后再发布",true));
+				if (time > DateTime.Now.AddMinutes(10)) throw new ActionStatusMessageException(new ApiResult(ActionStatusMessage.ApplyMessage.Operation.Submit.Crash,$"请{Math.Floor(time.Subtract(DateTime.Now.AddMinutes(10)).TotalSeconds)}秒后再发布",true));
 				else if (time.AddMinutes(1) > DateTime.Now)
 					appSetting.LastSubmitApplyTime = DateTime.Now.AddMinutes(20);
 				else
