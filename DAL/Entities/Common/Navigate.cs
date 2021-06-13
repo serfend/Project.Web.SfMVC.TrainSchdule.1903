047b95f36cced9e326dfd584382bbfc5e9d79f8a
汉广 <serfend@foxmail.com>
2021-06-13 19:44:15 +0800

    feat[AppNav]navigator init

---
+++ DAL/Entities/Common/Navigate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Common
+{
+    /// <summary>
+    /// 导航
+    /// </summary>
+    public class CommonNavigate:BaseEntityGuid
+    {
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        /// <summary>
+        /// 使用远程图片
+        /// </summary>
+        public string Icon { get; set; }
+        /// <summary>
+        /// 使用现有图标 Icon与此项应只设置一个
+        /// </summary>
+        public string Svg { get; set; }
+        public string Url { get; set; }
+    }
+
+}
---
+++ TrainSchdule/Controllers/Common/NavigationController.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainSchdule.Controllers.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class NavigationController {
+    }
+    /// <summary>
+    /// 菜单管理
+    /// </summary>
+
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public partial class NavigationController:Controller
+    {
+        private readonly ApplicationDbContext context;
+        /// <summary>
+        /// 
+        /// </summary>
+        public NavigationController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
