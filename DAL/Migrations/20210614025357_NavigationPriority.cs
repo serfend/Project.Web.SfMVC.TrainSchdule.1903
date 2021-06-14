1870a189fce592fbffdd619c6d6ab73dc138bfb3
汉广 <serfend@foxmail.com>
2021-06-14 14:12:21 +0800

    feat[navigate]support priority order

---
+++ DAL/Migrations/20210614025357_NavigationPriority.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class NavigationPriority : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "CommonNavigates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "CommonNavigates");
+        }
+    }
+}
