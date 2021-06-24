6237b7091cebb5770d8a1eef3d7e9682ef5d1a07
汉广 <serfend@foxmail.com>
2021-06-24 14:38:09 +0800

    feat[client-relate]log relate process

--- BLL/Crontab/ClientRelateJob.cs
+++ BLL/Crontab/ClientRelateJob.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Data;
+﻿using BLL.Interfaces;
+using DAL.Data;
 using DAL.Entities.ClientDevice;
 using DAL.Entities.UserInfo;
 using Newtonsoft.Json;
@@ -17,10 +18,12 @@ namespace BLL.Crontab
     public class ClientRelateJob : ICrontabJob
     {
         private readonly ApplicationDbContext context;
+        private readonly IUserActionServices userActionServices;
 
-        public ClientRelateJob(ApplicationDbContext context)
+        public ClientRelateJob(ApplicationDbContext context, IUserActionServices userActionServices)
         {
             this.context = context;
+            this.userActionServices = userActionServices;
         }
         public void RelateClientCompany()
         {
@@ -28,10 +31,11 @@ namespace BLL.Crontab
                 .Where(c => c.CompanyCode == null)
                 .Where(c => c.Ip != null)
                 .Where(c => c.Ip != string.Empty)
-                .Take(10).ToList();
+                .Take(100).ToList();
             RelateProperty(clients, (prev, refererTo) =>
             {
                 prev.CompanyCode = refererTo.CompanyCode;
+                userActionServices.Log(UserOperation.UserInfoUpdate, refererTo.Ip, $"绑定到:[{prev.CompanyCode}]", true);
             });
         }
         public void RelateClientMajor()
@@ -40,7 +44,7 @@ namespace BLL.Crontab
                 .Where(c => c.OwnerId == null)
                 .Where(c => c.Ip != null)
                 .Where(c => c.Ip != string.Empty)
-                .Take(10).ToList();
+                .Take(100).ToList();
             var companies = clients
                 .Where(c => c.CompanyCode != null)
                 .Select(c => c.CompanyCode)
@@ -54,16 +58,23 @@ namespace BLL.Crontab
                 }).ToDictionary(c => c.Key);
             foreach (var c in clients)
             {
-                var nacUserCheck = CheckNacUser(c.FutherInfo);
-                c.Owner = nacUserCheck ?? (c.CompanyCode != null ? companies[c.CompanyCode].Value : null);
+                var nacUserCheck = CheckNacUser(c.FutherInfo, out var userNac);
+                if (nacUserCheck != null)
+                    c.Owner = nacUserCheck;
+                else if (c.CompanyCode != null)
+                    c.Owner = companies[c.CompanyCode].Value;
+                if (c.Owner != null)
+                    userActionServices.Log(UserOperation.UserInfoUpdate, c.Ip, $"{userNac}[{c.CompanyCode}]绑定到:{c.Owner.BaseInfo.RealName}", true);
             }
         }
-        private User CheckNacUser(string nacInfo)
+        private User CheckNacUser(string nacInfo, out string nacUser)
         {
+            nacUser = null;
             if (nacInfo == null || nacInfo.Length < 10) return null;
             var item = JsonConvert.DeserializeObject(nacInfo) as JObject;
             if (item == null) return null;
             var userid = item.GetValue("nac_user").ToString();
+            nacUser = userid;
             userid = Regex.Replace(userid, @"\d", "");
             var userGroups = userid.Split('_');
             var db = context.AppUserBaseInfos;
@@ -79,13 +90,13 @@ namespace BLL.Crontab
             for (var i = max_length; i >= 4; i--)
             {
                 if (!dict.ContainsKey(i)) continue;
-                if (!dict.ContainsKey(i-1))
-                    dict.Add(i-1, new List<string>());
+                if (!dict.ContainsKey(i - 1))
+                    dict.Add(i - 1, new List<string>());
                 foreach (var g in dict[i])
                 {
                     var user = db.FirstOrDefault(i => i.PinYin.Contains(g));
                     if (user != null) return context.AppUsersDb.FirstOrDefault(u => u.BaseInfoId == user.Id);
-                    dict[i - 1].Add(g.Substring(1));              
+                    dict[i - 1].Add(g.Substring(1));
                 };
             }
             return null;
--- DAL/Entities/UserInfo/UserAction.cs
+++ DAL/Entities/UserInfo/UserAction.cs
@@ -6,69 +6,70 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Entities.UserInfo
 {
-	public enum ActionRank
-	{
-		Debug = 32,
-		Infomation = 16,
-		Warning = 8,
-		Danger = 4,
-		Disaster = 0
-	}
+    public enum ActionRank
+    {
+        Debug = 32,
+        Infomation = 16,
+        Warning = 8,
+        Danger = 4,
+        Disaster = 0
+    }
 
-	public class UserAction : BaseEntityGuid, ICreateClientInfo
-	{
-		public string UserName { get; set; }
+    public class UserAction : BaseEntityGuid, ICreateClientInfo
+    {
+        public string UserName { get; set; }
 
-		/// <summary>
-		/// 操作内容
-		/// </summary>
-		public UserOperation Operation { get; set; }
+        /// <summary>
+        /// 操作内容
+        /// </summary>
+        public UserOperation Operation { get; set; }
 
-		/// <summary>
-		/// 日志等级
-		/// </summary>
-		public ActionRank Rank { get; set; }
+        /// <summary>
+        /// 日志等级
+        /// </summary>
+        public ActionRank Rank { get; set; }
 
-		/// <summary>
-		/// 操作发生的时间
-		/// </summary>
-		public DateTime Date { get; set; }
+        /// <summary>
+        /// 操作发生的时间
+        /// </summary>
+        public DateTime Date { get; set; }
 
-		/// <summary>
-		/// 操作是否成功
-		/// </summary>
-		public bool Success { get; set; }
+        /// <summary>
+        /// 操作是否成功
+        /// </summary>
+        public bool Success { get; set; }
 
-		/// <summary>
-		/// 操作描述
-		/// </summary>
-		public string Description { get; set; }
+        /// <summary>
+        /// 操作描述
+        /// </summary>
+        public string Description { get; set; }
 
-		public string Ip { get; set; }
-		public string Device { get; set; }
-		public string UA { get; set; }
-	}
+        public string Ip { get; set; }
+        public string Device { get; set; }
+        public string UA { get; set; }
+    }
 
-	public enum UserOperation
-	{
-		Default = 0,
-		Register = 1,
-		Remove = 2,
-		Restore = 10,
-		ModifyUser = 3,
-		Login = 4,
-		Logout = 5,
-		ModifyPsw = 8,
-		CreateApply = 16,
-		RemoveApply = 17,
-		AuditApply = 18,
-		ModifyApply = 19,
-		AttachInfoToApply = 20,
-		Permission = 32,
-		FromUserReport = 64,
-		FromSystemReport = 65,
-		FileInspect = 128,
-		InvalidModel=192,
-		UpdateInfo = 194
-	}
+    public enum UserOperation
+    {
+        Default = 0,
+        Register = 1,
+        Remove = 2,
+        Restore = 10,
+        ModifyUser = 3,
+        Login = 4,
+        Logout = 5,
+        ModifyPsw = 8,
+        CreateApply = 16,
+        RemoveApply = 17,
+        AuditApply = 18,
+        ModifyApply = 19,
+        AttachInfoToApply = 20,
+        Permission = 32,
+        FromUserReport = 64,
+        FromSystemReport = 65,
+        UserInfoUpdate = 72,
+        FileInspect = 128,
+        InvalidModel = 192,
+        UpdateInfo = 194
+    }
 }
